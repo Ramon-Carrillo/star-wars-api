cd357feae93adfc3115c844274a63be4dbf75bea
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-06-01 19:56:12 -0500

    added html,css,and js for the api call that places name and image in a grid format.

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,23 @@
 'use strict'
 
 let apiUrl = `https://akabab.github.io/starwars-api/api/all.json`
+let main = document.getElementById('main')
 
 let totalCharacters = async () => {
   let response = await fetch(apiUrl)
   let data = await response.json()
   let character = await data
   character.map((char) => {
-    console.log(char.id)
+    const card = document.createElement('div')
+    const picture = document.createElement('img')
+    const name = document.createElement('h3')
+
+    picture.src = char.image
+    picture.alt = char.name
+    name.innerText = char.name
+    card.classList.add('card')
+    card.append(picture, name)
+    main.appendChild(card)
   })
 }
 
